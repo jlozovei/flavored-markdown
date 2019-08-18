6254430eff69517af2f51bb5ad0f288787a608e3
jlozovei <jlozovei@gmail.com>
2019-08-18 18:34:27 -0300

    overwrite vue-cli default configs
    - configure custom publicPath
    - create alias for "components" and "containers"

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+module.exports = {
+  publicPath: "./",
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src/"),
+        components: path.resolve(__dirname, "src/components"),
+        containers: path.resolve(__dirname, "src/containers")
+      }
+    }
+  }
+};
