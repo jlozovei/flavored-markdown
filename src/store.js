9e8a44d3d2e75dada744a0edda13b3d8dfb362cb
jlozovei <jlozovei@gmail.com>
2019-08-18 18:26:11 -0300

    add store structure

--- src/store.js
+++ src/store.js
@@ -4,7 +4,44 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    editor: {
+      markdown: "",
+      html: ""
+    }
+  },
+
+  mutations: {
+    setMarkdown(state, payload) {
+      state.editor.markdown = payload;
+    },
+
+    setHTML(state, payload) {
+      state.editor.html = payload;
+    }
+  },
+
+  actions: {
+    storeMarkdown({ commit }, payload) {
+      commit("setMarkdown", payload.value);
+    },
+
+    storeHTML({ commit }, payload) {
+      commit("setHTML", payload.value);
+    }
+  },
+
+  getters: {
+    editor(state) {
+      return state.editor;
+    },
+
+    markdown(state) {
+      return state.editor.markdown;
+    },
+
+    html(state) {
+      return state.editor.html;
+    }
+  }
 });
